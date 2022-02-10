6e26a9f3993e5d369c7a55f3e6a0b5bc15830e8f
Leonardo <odriozola-leo@hotmail.com>
2022-02-09 22:31:55 -0300

    casi terminando

--- src/main/java/Logica/CPU.java
+++ src/main/java/Logica/CPU.java
@@ -23,9 +23,12 @@ public class CPU implements ICPU{
         return instance;
     }
 
+    /**
+     * Este metodo agrega las subrutinas entrantes a un objetosu subrutina
+     * @param subrutinas
+     */
     @Override
     public void agregarSubrutina(String[] subrutinas) {
-
         for(String subrutina: subrutinas){
             String[] partes = subrutina.split(" ");
             Subrutina subr = new Subrutina(partes);
@@ -34,24 +37,60 @@ public class CPU implements ICPU{
         }
     }
 
+    private int getIndex(String registro){
+        if(registro.charAt(0) == 'R'){
+            String s = registro.substring(1);
+
+            if(s.charAt(0) == '0')
+                return Integer.parseInt(s.substring(1));
+
+            return Integer.parseInt(s);
+        }
+        /*
+        if(registro.contains(".")){
+            return Integer.(registro.charAt(0));
+        }
+        */
+        return Integer.parseInt(registro);
+    }
+
     @Override
-    public void ejecutarSubrutinas() {
+    public void ejecutarSubrutinas() { // :O
         for(int i = 0; i < this.subrutinas.size(); i++){
             Subrutina subr = (Subrutina)this.subrutinas.get(i);
+
             switch(subr.getComando()){
                 case "MOV":
+                    String param = (String)subr.getParametros().get(0);
+                    if(param.charAt(0) == 'R'){
+                        registros[0] = 3;
+
+                        MOV(getIndex((String)subr.getParametros().get(0)),getIndex((String)subr
+                        .getParametros().get(1)));
+                        //System.out.println(registros[2]);
+                    }else{
+                        MOV((String)subr.getParametros().get(0), getIndex((String)subr
+                        .getParametros().get(1)));
+                        //System.out.println(registros[1]);
+                    }
                     break;
                 case "ADD":
+                    ADD(getIndex((String)subr.getParametros().get(0)), getIndex((String)subr.getParametros().get(1)));
                     break;
                 case "DEC":
+                    DEC(getIndex((String)subr.getParametros().get(0)));
                     break;
                 case "INC":
+                    INC(getIndex((String)subr.getParametros().get(0)));
                     break;
                 case "INV":
+                    INV(getIndex((String)subr.getParametros().get(0)));
                     break;
                 case "JMP":
+                    i = JMP(getIndex((String)subr.getParametros().get(0)));
                     break;
-                case "JS":
+                case "JZ":
+
                     break;
                 case "NOP":
                     break;
@@ -61,22 +100,70 @@ public class CPU implements ICPU{
 
 
     private void MOV(int Rxx, int Ryy) {
-        Ryy = Rxx;
-
+        registros[Ryy] = registros[Rxx];
     }
 
-    private void MOV(float d, int Rxx) {
-
+    private void MOV(String d, int Rxx) {
+        registros[Rxx] = getIndex(d);
     }
+    private void ADD(int Rxx, int Ryy) {
+            registros[Rxx] = (int) ((registros[Rxx] + registros[Ryy])); /* % Math.pow(2, 32)) */;
+        }
+    private void DEC(int Rxx) {
+            if(registros[Rxx]==0){
+                registros[Rxx] = (int)Math.pow(2,32) -1;
+            }
+            registros[Rxx] = registros[Rxx]-1;
+        }
+    private void INC(int Rxx) {
+            if(registros[Rxx] >= Math.pow(2, 32)-1) registros[Rxx] = 0;
+            registros[Rxx] = registros[Rxx]+1;
+        }
 
-    private int getIndex(String registro){
-        String s = registro.substring(1);
-        if(s.charAt(0) == '0') return Integer.parseInt(s.substring(1));
-        return Integer.parseInt(s);
-    }
-    private int JZ (int d){
-        if (registros[0]==0){
-            return d-1;
+    private void INV(int Rxx){
+
+        String Binario = Integer.toBinaryString(Rxx);
+        System.out.println(Binario);
+        String NewBinario = "" ;
+        int[] arr = Arrays.stream(Binario.substring(0, Binario.length()).split(""))
+                .map(String::trim).mapToInt(Integer::parseInt).toArray();
+
+
+
+        for (int i = 0; i< arr.length; i++){
+            if (arr[i]==0){
+                arr[i]=1;
+            }
+            else
+                arr[i]=0;
+
+            NewBinario= arr[i] + NewBinario;
+
+        }
+
+        NewBinario = new StringBuilder(NewBinario).reverse().toString();
+        int NewRxx = Integer.parseInt(NewBinario);
+        NewRxx= Integer.parseInt(NewBinario,2);
+        System.out.println(NewRxx);
+
         }
     }
+
+
+    private int JMP(int d){
+        return d;
+    }
+
+    private void JZ(int d){
+        if(registros[0] == 0){
+            JMP(d);
+        }
+    }
+   /* private int JZ (int d){
+
+        if (parametros[0]==0){
+            return d-1;
+        }
+        return 1;
+    }*/
 }
--- src/main/java/Logica/Subrutina.java
+++ src/main/java/Logica/Subrutina.java
@@ -1,20 +1,28 @@
 package Logica;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Subrutina {
     private String comando;
     private List<String> parametros;
     
-    public Subrutina(String[] subrutina) {
+    public Subrutina(String[] subrutina) { //Aquí entra UNA instrucción con sus parametros
         this.comando = subrutina[0];
         this.parametros = new ArrayList<>();
-        
-        for(int i = 1; i < subrutina.length; i++){
-            this.parametros.add(subrutina[i]);
+        String[] parametro = subrutina[1].split(",");
+
+        for(int i =0; i < parametro.length; i++){
+            this.parametros.add(parametro[i]);
+        }
+
+        Iterator it = this.parametros.iterator();
+        while(it.hasNext()){
+            System.out.println((String)it.next());
         }
     }
-    
+
     public String getComando() {
         return comando;
     }
